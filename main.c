e0d1c2f7aba5622ab6bf1fa0033abe988f1868c0
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-03-18 05:52:00 +0000

    update the add a new task function

--- main.c
+++ main.c
@@ -1,22 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void display_the_todo_lists(){
+void display_the_todo_lists()
+{
 
-    printf("call the function that display the to-do list titels that are existes ");
+    printf("call the function that display the to-do list titls that are existes ");
 
 }
 
-void create_a_todo_list(){
+void create_a_todo_list()
+{
     char *title = NULL;
+    char *task = NULL;
+    int priority ;
+    int duration;
+
     title = (char*)malloc(sizeof(char) * 50);
+    if (title == NULL)
+    {
+        printf("Memory allocation failed for title.\n");
+        return;
+    }
+    task = (char*)malloc(sizeof(char) * 50);
+    if (task == NULL)
+    {
+        printf("Memory allocation failed for task.\n");
+        free(title); // Free allocated memory for title before returning
+        return;
+    }
+
+
     printf("\n--- create a new to-do list ---\n\n");
-    // printf("-> What would you like to call your to-do list?");
+
+    // get the title of the to-do list
     printf("-> Enter a title that reflects your goals for this to-do list : ");
-    scanf("%s", title);
+      //fgets(title, 49, stdin);
+      //getchar();
+    scanf(" %[^\n]", title);
 
-    printf("the title is %s", title);
+    // start geting the tasks of the todo list
+    printf("\n-> Please describe your task: ");
+    //fgets(task, 49, stdin);
+    //getchar();
+    scanf(" %[^\n]", task);
 
+    printf("\n-> What is it's level of priority (1,2,3,4) : ");
+    scanf("%d", &priority);
+
+    printf("\n-> How long did you anticipate this would take to complete in minutes? : ");
+    scanf("%d", &duration);
+
+    printf("\nthe title is %s\n", title);
+    printf("the task is %s\n", task);
+    printf("the priority is %d\n", priority);
+    printf("the duration is %d\n", duration);
 
 }
 
@@ -47,7 +84,8 @@ askTheUserToChoose:
     printf("\n-> What would you like to do next? : ");
     scanf("%d", &userOptionChoice);
 
-    switch(userOptionChoice){
+    switch(userOptionChoice)
+    {
     case 1:
         display_the_todo_lists();
         break;
@@ -65,25 +103,25 @@ askTheUserToChoose:
         goto askTheUserToChoose;
 
     }
-   /* // check if the user want to create a new to-do list or not
-    if(userResponseToCreateNewList == 'Y' || userResponseToCreateNewList == 'y')
-    {
+    /* // check if the user want to create a new to-do list or not
+     if(userResponseToCreateNewList == 'Y' || userResponseToCreateNewList == 'y')
+     {
 
-        // call the function that is responsible for creating new to-do listes
-        printf("starting creating a new to-do list...");
-    }
-    else if(userResponseToCreateNewList == 'N' || userResponseToCreateNewList == 'n')
-    {
+         // call the function that is responsible for creating new to-do listes
+         printf("starting creating a new to-do list...");
+     }
+     else if(userResponseToCreateNewList == 'N' || userResponseToCreateNewList == 'n')
+     {
 
-        // show the existing to do lists.
-        printf("no to-do list will be created");
-    }
-    else
-    {
-        // handle invalid inputs
-        printf("Please enter 'Y' for yes or 'N' for no.");
-        goto askTheUserToCreatNewToDo;
-    }
-    */
+         // show the existing to do lists.
+         printf("no to-do list will be created");
+     }
+     else
+     {
+         // handle invalid inputs
+         printf("Please enter 'Y' for yes or 'N' for no.");
+         goto askTheUserToCreatNewToDo;
+     }
+     */
     return 0;
 }
