35c89d3f9bdf0ebbe3ddada1f13267f16740b273
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-03-19 13:37:44 +0000

    add functions and structure

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int minites_To_hours(int min){
+typedef struct{
+    char description[20];
+    int Priority;
+    int expacted_duration;
+}todo_task;
+
+// structure for the To-do list that will host the title of it and the tasks on it
+typedef struct{
+    char title[50];
+    int number_of_tasks ;
+
+    todo_tasks todo_task[5];
+
+}todo_list;
+
+
+
+int minites_To_hours(int min)
+{
     int minites = min%60;
     int hours = min/60;
 
@@ -19,7 +37,7 @@ void display_the_todo_list_items()
 
     printf("this function display the to-do list items that are exist.");
 
-       /*
+    /*
     printf("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
     printf("=> the title is : %s\n", title);
     printf("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n");
@@ -38,9 +56,9 @@ void display_the_todo_list_items()
 void display_the_todo_list_item_details()
 {
 
-    printf("this function display the to-do list item details and provide options to edit it.");
+    printf("This function display the to-do list item details and provide options to edit it.");
 
-       /*
+    /*
     printf("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
     printf("=> the title is : %s\n", title);
     printf("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n");
@@ -54,7 +72,7 @@ void display_the_todo_list_item_details()
     printf("=> Option 1: Mark To-Do item as Complete\n");
     printf("=> Option 2: Create a New To-Do item\n");
     printf("=> Option 3: Delete To-Do item\n");
-    printf("=> Option 4: go to the prevuse page\n");
+    printf("=> Option 4: go back to the prevuse page\n");
     printf("=> Option 5: go to the firs menu\n");
 
 
@@ -62,6 +80,7 @@ void display_the_todo_list_item_details()
 
 void add_new_tasks_to_the_todo_list()
 {
+    todo_list todoList[5];
 
     char *task = NULL;
     int priority ;
@@ -75,7 +94,7 @@ void add_new_tasks_to_the_todo_list()
     }
 
 
-    printf("\n--- create a new to-do list ---\n\n");
+    printf("\n--- Create a new to-do list ---\n\n");
 
 
 
@@ -84,8 +103,8 @@ void add_new_tasks_to_the_todo_list()
     printf("\n-> Please describe your task: ");
     scanf(" %[^\n]", task);
 
-    //fgets(task, 49, stdin);
     //getchar();
+    //fgets(task, 49, stdin);
 
     printf("\n-> What is it's level of priority (1,2,3,4) : ");
     scanf("%d", &priority);
@@ -93,17 +112,26 @@ void add_new_tasks_to_the_todo_list()
     printf("\n-> How long did you anticipate this would take to complete in minutes? : ");
     scanf("%d", &duration);
 
+    todo_list.todo_task[0];
+    todo_list.todo_tasks[todoList[0].number_of_tasks].Priority = priority;
+    todo_list.todo_tasks[todoList[0].number_of_tasks].expacted_duration = duration;
+
+    /*
     printf("=> the task is : %s\n", task);
     printf("=> the priority is : %d\n", priority);
     printf("=> the duration is : %d\n", duration);
+    */
+    printf("The task was add sucssfuly");
 
 }
 
-void create_a_new_todo_list()
+void create_a_new_todo_list(FILE* fptr)
 {
+    todo_list todoList[5];
     char *title = NULL;
+    //char filename[64];
     char userResponseToCreateNewTask;
-
+    //FILE *new_todo_list;
 
     title = (char*)malloc(sizeof(char) * 50);
     if (title == NULL)
@@ -115,8 +143,17 @@ void create_a_new_todo_list()
     printf("-> Enter a title that reflects your goals for this to-do list : ");
     scanf(" %[^\n]", title);
 
-    //fgets(title, 49, stdin);
-    //getchar();
+    todoList[0].title = title;
+    todoList[0].number_of_tasks= -1;
+
+
+    //fprintf(fptr, title);
+    //fprintf(fptr, "\n");
+    //sprintf(filename, "%s.txt", title);
+
+
+    //new_todo_list = fopen(filename, "a");
+
 
 
     printf("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
@@ -133,6 +170,7 @@ void create_a_new_todo_list()
 
             // call the function that is responsible for creating new to-do listes
             printf("starting creating a new to-do Task...");
+            todoList[0].number_of_tasks= todoList[0].number_of_tasks +1;
             add_new_tasks_to_the_todo_list();
         }
         else if(userResponseToCreateNewTask == 'N' || userResponseToCreateNewTask == 'n')
@@ -151,23 +189,25 @@ void create_a_new_todo_list()
     }
     while(1);
 
+    //fclose(new_todo_list);
     free(title); // Free allocated memory for title before returning
 }
 
 int main()
 {
+    FILE *fptr;
+    fptr = fopen("to-do_list_titles.txt", "a");
     int userOptionChoice;
 
     // starting the app
     printf("#### TODO LIST APP ####\n\n");
 
-    // welcome the user
-    //printf("Hello user!\n\n");
+
 
     //display the start menu
 askTheUserToChoose:
     printf("=> Option 1: View To-Do List\n");
-    printf("=> Option 2: create a New To-Do List\n");
+    printf("=> Option 2: Create a New To-Do List\n");
     printf("=> Option 3: Exit\n");
 
     /* after choosing the first option
@@ -187,7 +227,7 @@ askTheUserToChoose:
         display_the_todo_lists();
         break;
     case 2:
-        create_a_new_todo_list();
+        create_a_new_todo_list(fptr);
         break;
     case 3:
         printf("the app is closed.");
@@ -200,6 +240,6 @@ askTheUserToChoose:
         goto askTheUserToChoose;
 
     }
-
+    fclose(fptr);
     return 0;
 }
